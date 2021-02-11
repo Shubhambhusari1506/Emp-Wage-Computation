a71e5ded1da34c61dd6d45fd442f7e6f0066f181
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-11 17:58:02 +0530

    Added:wages till condition total work hrs

---
+++ wagestotalworkhrs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+#constants
+EMP_RATE_PER_HR=20
+IS_FULL_TIME=1
+IS_PART_TIME=2
+NUM_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+
+#variable 
+totalEmpHrs=0
+totalworkingDays=0
+
+
+function getworkHrs() {
+		local empCheck=$1
+		case $empCheck in 
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+			*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&  $totalworkingDays -lt $NUM_OF_WORKING_DAYS ]]
+do 
+
+		((totalworkingDays++))
+		empCheck=$(( RANDOM%3 ))
+		empHrs="$( getworkHrs $empCheck)"
+		totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalsalary=$(($totalEmpHrs*$EMP_RATE_PER_HR)) 
